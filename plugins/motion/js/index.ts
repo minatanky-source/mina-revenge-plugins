973e0d8d94ee07f3e362067c4c25f649c4c7bebf
minatanky-source <minatanky@gmail.com>
2026-09-17 16:28:29 -0300

    fix(Motion): dedupe settings subscription

--- plugins/motion/js/index.ts
+++ plugins/motion/js/index.ts
@@ -3,16 +3,29 @@ import { DEFAULTS, setStorage } from './state'
 import { SettingsComponent } from './ui'
 import type { MotionSettings } from './state'
 
+let activeStorageUnsubscribe: (() => unknown) | undefined
+
 export default plugin<{ jsonStorage: MotionSettings }>({
 	jsonStorage: { default: DEFAULTS, load: true },
 	start(api) {
+		activeStorageUnsubscribe?.()
 		setStorage(api.jsonStorage)
+
 		const settingsChanged = installMotion(api.cleanup)
-		api.cleanup(api.jsonStorage.subscribe(settingsChanged))
+		const unsubscribe = api.jsonStorage.subscribe(settingsChanged)
+		activeStorageUnsubscribe = unsubscribe
+		api.cleanup(() => {
+			if (activeStorageUnsubscribe === unsubscribe)
+				activeStorageUnsubscribe = undefined
+			unsubscribe()
+		})
+
 		if (api.plugin.startedLate) api.plugin.requireReload()
 		return api.jsonStorage.get()
 	},
 	stop() {
+		activeStorageUnsubscribe?.()
+		activeStorageUnsubscribe = undefined
 		setStorage(undefined)
 	},
 	SettingsComponent,
